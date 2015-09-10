277b777f6052721a8cd1afb3174b4b769ab6a750
dtroiani <dtroiani@danielet-PC.adrialab.intra>
2015-09-10 17:00:18 +0200

    Estratta superclasse AbstractFileRepositoryBustaPaga

--- src/it/dtroiani/bustapaga/FileRepositoryBustaPagaConsulente.java
+++ src/it/dtroiani/bustapaga/FileRepositoryBustaPagaConsulente.java
@@ -5,18 +5,11 @@
  */
 package it.dtroiani.bustapaga;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author DanieleT
  */
-public class FileRepositoryBustaPagaConsulente implements IRepositoryBustaPaga {
+public class FileRepositoryBustaPagaConsulente extends AbstractFileRepositoryBustaPaga {
 
     private final PagamentoConsulente bustaPaga;
 
@@ -25,53 +18,37 @@ public class FileRepositoryBustaPagaConsulente implements IRepositoryBustaPaga {
     }
 
     @Override
-    public void registra() {
-        FileWriter fw = null;
-        try {
-            // pattern nome file yyyymm-cognome-nome.txt (esempio: 201212-rossi-mario.txt)
-            StringBuilder nomeFile = new StringBuilder();
-            nomeFile.append(bustaPaga.getAnno()).
-                    append(bustaPaga.getMese()).
-                    append("-").
-                    append(bustaPaga.getRisorsa().getCognome()).
-                    append("-").
-                    append(bustaPaga.getRisorsa().getNome()).
-                    append(".txt");
-
-            StringBuilder contenuto = new StringBuilder();
-            // intestazione
-            contenuto.append(bustaPaga.getRisorsa().getMatricola()).
-                    append(" | ").
-                    append(bustaPaga.getRisorsa().getCognome()).
-                    append(" | ").append(bustaPaga.getRisorsa().getNome());
-            contenuto.append("\n");
-            // corpo
-            contenuto.append("Giorni lavorati: ").append(bustaPaga.getNumGiorniLavorati()).
-                    append(" * ").
-                    append(PagamentoConsulente.COMPENSO_ORARIO_ORDINARIO).
-                    append(" = ").
-                    append(bustaPaga.getTotCompenso());
-            // scrittura su file txt
-            File file = new File(nomeFile.toString());
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            fw = new FileWriter(file.getAbsoluteFile());
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(contenuto.toString());
-            bw.close();
-            System.out.println("----------");
-            System.out.println(contenuto.toString());
-            System.out.println("----------");
-        } catch (IOException ex) {
-            Logger.getLogger(FileRepositoryBustaPagaConsulente.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                fw.close();
-            } catch (IOException ex) {
-                Logger.getLogger(FileRepositoryBustaPagaConsulente.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+    String preparaIntestazione() {
+        StringBuilder intestazione = new StringBuilder();
+        intestazione.append(bustaPaga.getRisorsa().getMatricola()).
+                append(" | ").
+                append(bustaPaga.getRisorsa().getCognome()).
+                append(" | ").append(bustaPaga.getRisorsa().getNome());
+        return intestazione.toString();
+    }    
+    
+    @Override
+    String preparaCorpo() {
+        StringBuilder corpo = new StringBuilder();
+        corpo.append("Giorni lavorati: ").append(bustaPaga.getNumGiorniLavorati()).
+                append(" * ").
+                append(PagamentoConsulente.COMPENSO_ORARIO_ORDINARIO).
+                append(" = ").
+                append(bustaPaga.getTotCompenso());
+        return corpo.toString();
     }
 
+    @Override
+    String impostaNomeFile() {
+        StringBuilder nomeFile = new StringBuilder();
+        nomeFile.append(bustaPaga.getAnno()).
+                append(bustaPaga.getMese()).
+                append("-").
+                append(bustaPaga.getRisorsa().getCognome()).
+                append("-").
+                append(bustaPaga.getRisorsa().getNome()).
+                append(".txt");
+        return nomeFile.toString();
+    }
+ 
 }
